64c274dc52dd24ca4353fdec05e05fdf63e8a7ab
Martin Indra <martin.indra@mgn.cz>
2024-04-19 18:28:16 +0200

    WIP

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -9,6 +9,26 @@ class Board:
         num_positions = self._width * self._height
         self._positions = [0] * num_positions
 
+    def set_position(self, x: int, y: int, player: int):
+        # TODO extend board if out of range
+        x_offset, y_offset = self._to_offsets(x, y)
+
+        if x_offset < 0:
+            min_x = x - 10
+            x_shift = min_x - self._min_x
+            width = abs(x_shift) + self._width
+            positions = [0] * (width * self._height)
+
+            for i in range(len(self._positions)):
+                x = i // self._width + x_shift
+                y = i - (i // self._width)
+                index = y * width + x
+                positions[index] = self._positions[i]
+
+            self._positions = positions
+
+        # TODO set the position
+
     def get_range(self, x: int, y: int, width: int, height: int):
         result = []
         for dy in range(height):
